698d09d74c6d43e1fda23c80beac1ff37926885d
Maddy <maddy@savaslabs.com>
2025-01-23 13:29:56 -0600

    #3: Update message copy

--- lib/harvest_notifier/templates/daily_report.rb
+++ lib/harvest_notifier/templates/daily_report.rb
@@ -5,9 +5,9 @@ require "harvest_notifier/templates/base"
 module HarvestNotifier
   module Templates
     class DailyReport < Base
-      REMINDER_TEXT = "*Guys, don't forget to report the working hours in Harvest every day.*"
-      USERS_LIST_TEXT = "Here is a list of people who didn't report the working hours for *%<current_date>s*:"
-      REPORT_NOTICE_TEXT = "_Please, report time and react with :heavy_check_mark: for this message._"
+      REMINDER_TEXT = "*Don't forget to log your hours in Harvest every day. It is crucial to our business.*"
+      USERS_LIST_TEXT = "Here is a list of people who didn't log time for *%<current_date>s*:"
+      REPORT_NOTICE_TEXT = "_Log your time now and react with_ :white_check_mark:"
       SLACK_ID_ITEM = "• <@%<slack_id>s>"
       FULL_NAME_ITEM = "• %<full_name>s"
 
@@ -61,7 +61,7 @@ module HarvestNotifier
                   json.style "primary"
                   json.text do
                     json.type "plain_text"
-                    json.text "Report Time"
+                    json.text "Log Time"
                   end
                 end
 
@@ -82,7 +82,7 @@ module HarvestNotifier
       private
 
       def formatted_date
-        assigns[:date].strftime("%B%eth")
+        assigns[:date].strftime("%A, %B %d")
       end
 
       def refresh_value
